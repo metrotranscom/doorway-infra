ade179d3aed625fe2a2ec54cb12422a0f4bcaf5c
Chris Casto <chriscasto@google.com>
2023-02-14 16:58:44 -0800

    initialize TF for bloom instance (#12)
    Fixes #1

---
+++ bloom-instance/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "doorway-state"
+    key    = "bloom/default"
+    region = "us-west-1"
+  }
+}
+
+provider "aws" {
+  # TODO: replace with var
+  region = "us-west-1"
+}
