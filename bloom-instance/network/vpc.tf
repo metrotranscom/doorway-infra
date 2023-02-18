f730c1a5007bb2200f151f9f0055b8b67ca2d155
Liann Sun <lianns@google.com>
2023-02-18 00:40:06 +0000

    chore: remove cruft comment line

--- bloom-instance/network/vpc.tf
+++ bloom-instance/network/vpc.tf
@@ -1,4 +1,3 @@
-# Not sure if I need this file.
 # The VPC acts as our own dedicated slice of the cloud
 # It's the root of our network and where most of our resources will reside
 resource "aws_vpc" "vpc" {
