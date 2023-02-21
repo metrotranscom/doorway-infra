f71a41aa50def50838e4dac5e26ec5cec2d54e03
Liann Sun <lianns@google.com>
2023-02-21 14:38:44 -0800

    feat: add IGW (#22)

--- bloom-instance/vpc.tf
+++ bloom-instance/vpc.tf
@@ -4,3 +4,11 @@ resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
   tags       = local.default_tags_with_name
 }
+
+# The Internet Gateway (IGW) is what enables bidirectional communication
+# in our public subnet.  It's what makes the public subnet "public"
+resource "aws_internet_gateway" "igw" {
+  # Note that the IGW references the VPC we created above
+  vpc_id = aws_vpc.vpc.id
+  tags   = local.default_tags_with_name
+}
