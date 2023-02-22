8a4c00f54ecc6b209a18274e677e61b230e92db1
Guanghui Liu <ghliu@google.com>
2023-02-22 11:41:02 -0800

    fix the data subnets config

--- bloom-instance/subnets.tf
+++ bloom-instance/subnets.tf
@@ -29,18 +29,3 @@ resource "aws_subnet" "data" {
     local.default_tags
   )
 }
-
-resource "aws_subnet" "data" {
-  for_each = { for idx, subnet in var.subnet_map.data : idx => subnet }
-
-  vpc_id            = aws_vpc.vpc.id
-  availability_zone = each.value.az
-  cidr_block        = each.value.cidr
-
-  tags = merge(
-    {
-      Name = "${local.default_name}:data_subnet-${each.key}"
-    },
-    local.default_tags
-  )
-}
