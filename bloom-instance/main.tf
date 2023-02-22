d9d17d2ce3a2d70d5018b7c1fb3266a02678a876
Chris Casto <chriscasto@google.com>
2023-02-21 16:08:26 -0800

    fix: add workspace tag (#24)
    * add workspace tag
    
    * reformat default_name to incorporate workspace

--- bloom-instance/main.tf
+++ bloom-instance/main.tf
@@ -19,13 +19,16 @@ provider "aws" {
 }
 
 locals {
-  default_name = "${var.resource_prefix}-${var.sdlc_stage}-${var.application_name}"
+  # We may want to rearrange the order of these in the future based on how easy
+  # or hard it is to read at a glance in the console.
+  default_name = "${var.resource_prefix}:${terraform.workspace}-${var.sdlc_stage}:${var.application_name}"
 
   default_tags = {
     Team        = var.team_name
     Projects    = var.project_name
     Application = var.application_name
     Environment = var.sdlc_stage
+    Workspace   = terraform.workspace
   }
 
   default_tags_with_name = merge(
