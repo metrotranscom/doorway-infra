fd54e4fcfc8476602aa90b54ff5eb78992dd3f20
Inés Franch <inesfranch@google.com>
2023-02-23 12:50:23 -0800

    Remove S3 bucket ACL

--- bloom-instance/logging.tf
+++ bloom-instance/logging.tf
@@ -2,8 +2,3 @@ resource "aws_s3_bucket" "logging_bucket" {
   bucket_prefix = "${var.resource_prefix}-logging"
   tags          = local.default_tags_with_name
 }
-
-resource "aws_s3_bucket_acl" "logging_bucket_acl" {
-  bucket = aws_s3_bucket.logging_bucket.id
-  acl    = "private"
-}
