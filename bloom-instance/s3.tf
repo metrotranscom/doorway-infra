281168151ced2a25275a853d1ede431cebd39f23
Inés Franch <inesfranch@google.com>
2023-02-23 14:35:56 -0800

    Remove bucket ACL

--- bloom-instance/s3.tf
+++ bloom-instance/s3.tf
@@ -2,8 +2,3 @@ resource "aws_s3_bucket" "user_upload_bucket" {
   bucket_prefix = "${var.resource_prefix}-user-uploads"
   tags          = local.default_tags_with_name
 }
-
-resource "aws_s3_bucket_acl" "user_upload_bucket_acl" {
-  bucket = aws_s3_bucket.user_upload_bucket.id
-  acl    = "private"
-}
