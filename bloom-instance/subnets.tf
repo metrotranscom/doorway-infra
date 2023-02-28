b4b6c2354c94f0ef9f64cca884fe016be1152fef
Guanghui Liu <gl676c@gmail.com>
2023-02-28 15:02:26 -0800

    Route table for public subnets (#38)
    Fixes #11

--- bloom-instance/subnets.tf
+++ bloom-instance/subnets.tf
@@ -14,6 +14,33 @@ resource "aws_subnet" "public" {
   )
 }
 
+# Route table for the public subnets
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  # Local routes to VPC IPs are implicit, so we don't have to add those
+
+  # This routes all unmatched traffic out to the internet over the IGW
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(
+    {
+      Name = "${local.default_name}:public_subnet"
+    },
+    local.default_tags
+  )
+}
+
+# Association of the public subnet route table
+resource "aws_route_table_association" "pub_rt_assoc" {
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
 # Backend subnets
 resource "aws_subnet" "backend" {
   for_each = { for idx, subnet in var.subnet_map.backend : idx => subnet }
