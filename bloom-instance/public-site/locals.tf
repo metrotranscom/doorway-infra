1984be3107beb6eed2e31f976c25e41dc7040821
Chris Casto <chriscasto@google.com>
2023-03-10 17:11:56 +0000

    set bucket_prefix local

--- bloom-instance/public-site/locals.tf
+++ bloom-instance/public-site/locals.tf
@@ -1,4 +1,5 @@
 
 locals {
-  port = var.service_definition.port != null ? var.service_definition.port : 3000
+  port          = var.service_definition.port != null ? var.service_definition.port : 3000
+  bucket_prefix = "${var.name_prefix}/${var.service_definition.name}"
 }
