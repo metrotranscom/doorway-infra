0e46fcd7116ff0536f9eb37e86b23303e42f5a8b
Chris Casto <chriscasto@google.com>
2023-03-13 20:18:56 +0000

    add partner site

--- bloom-instance/main.tf
+++ bloom-instance/main.tf
@@ -66,6 +66,7 @@ module "public_alb" {
   }
 }
 
+# There may be multiple public sites
 module "public_sites" {
   for_each = { for idx, srv in var.public_sites : idx => srv }
   source   = "./services/public-site"
@@ -86,5 +87,28 @@ module "public_sites" {
   additional_tags = {
     ServiceType = "public-site"
     ServiceName = each.value.name
+  }
+}
+
+# So far, there only seems to be a need for a single partner site
+module "partner_site" {
+  source = "./services/partner-site"
+
+  name_prefix        = local.default_name
+  service_definition = var.partner_site
+
+  alb_listener_arn = module.public_alb.listeners.public.arn
+  alb_sg_id        = module.public_alb.security_group.id
+  subnet_ids       = [for subnet in module.network.subnets.app : subnet.id]
+
+  public_upload_bucket = aws_s3_bucket.user_upload_bucket.bucket
+  secure_upload_bucket = aws_s3_bucket.user_upload_bucket.bucket
+
+  # Just a placeholder for now
+  backend_api_base = "http://localhost:3100"
+
+  additional_tags = {
+    ServiceType = "partner-site"
+    ServiceName = var.partner_site.name
   }
 }
