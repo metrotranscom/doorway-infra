9dd3c81b93590e63ed84a0edce8508948136c326
Chris Casto <chriscasto@google.com>
2023-03-20 17:28:41 +0000

    consolidate files in dns/zone

--- bloom-instance/dns/zone/main.tf
+++ bloom-instance/dns/zone/main.tf
@@ -3,3 +3,10 @@ locals {
   create_zone = var.zone_id == null
   zone_id     = local.create_zone ? aws_route53_zone.zone["0"].zone_id : var.zone_id
 }
+
+resource "aws_route53_zone" "zone" {
+  count = local.create_zone ? 1 : 0
+  name  = var.name
+
+  tags = var.additional_tags
+}
