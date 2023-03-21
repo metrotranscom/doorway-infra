632d7fab67f6f559646c3a40eebb14b81456dbc8
Guanghui Liu <ghliu@google.com>
2023-03-21 10:19:48 -0700

    Rename directory

---
+++ app-codepipeline/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "doorway-state"
+    key    = "state/cicd.tfstate"
+    region = "us-west-1"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
