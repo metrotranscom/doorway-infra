e22bceb61ff445fa3a37a1d7f470599cd40bb615
Chris Casto <chriscasto@google.com>
2023-03-22 22:28:23 +0000

    add cert module instance and update tfvars.template

--- bloom-instance/certs.tf
+++ bloom-instance/certs.tf
@@ -1,7 +1,7 @@
 
 module "certs" {
   source   = "./cert"
-  for_each = { for key, cert in var.certs : key => cert }
+  for_each = { for cert in var.certs : cert.domain => cert }
 
   zones = module.dns.zone_map
   cert  = each.value
