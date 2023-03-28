f0522db6dea95549133f6a95b4e91230fbc73049
Chris Casto <chriscasto@google.com>
2023-03-28 20:03:42 +0000

    rename database dir to db

--- bloom-instance/db.tf
+++ bloom-instance/db.tf
@@ -1,6 +1,6 @@
 
 module "db" {
-  source      = "./database"
+  source      = "./db"
   name_prefix = local.default_name
   subnet_ids  = [for subnet in module.network.subnets.data : subnet.id]
   settings    = var.database
