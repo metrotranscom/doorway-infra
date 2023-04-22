78ea348e39d3e593b5cbb78c4797e4c122639814
Inés Franch <inesfranch@google.com>
2023-04-21 17:22:56 -0700

    feat: add S3 bucket CORS policy and bucket policy

--- bloom-instance/s3.tf
+++ bloom-instance/s3.tf
@@ -1,3 +1,40 @@
 resource "aws_s3_bucket" "user_upload_bucket" {
   bucket_prefix = "${var.name_prefix}-user-uploads"
 }
+
+resource "aws_s3_bucket_cors_configuration" "user_upload_cors_config" {
+  bucket = aws_s3_bucket.user_upload_bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "GET"]
+    allowed_origins = ["http://localhost:3001"]
+    expose_headers  = []
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  bucket = aws_s3_bucket.user_upload_bucket.id
+  policy = data.aws_iam_policy_document.allow_public_access_policy.json
+}
+
+data "aws_iam_policy_document" "allow_public_access_policy" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.user_upload_bucket.arn,
+      "${aws_s3_bucket.user_upload_bucket.arn}/*",
+    ]
+  }
+}
