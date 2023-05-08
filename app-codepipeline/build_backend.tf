efc69c8f298b6f2db795cb4e61395aab0659cdc7
isaac g <drumsound@google.com>
2023-05-08 19:44:44 +0000

    ci: parallel build in codepipeline

---
+++ app-codepipeline/build_backend.tf
@@ -0,0 +1,36 @@
+resource "aws_codebuild_project" "backend" {
+  name         = "${var.name_prefix}-codebuild-backend"
+  service_role = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:6.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+
+    dynamic "environment_variable" {
+      for_each = local.build_env_vars
+      content {
+        name  = environment_variable.value["name"]
+        value = environment_variable.value["value"]
+      }
+    }
+  }
+  logs_config {
+    cloudwatch_logs {
+      status = "ENABLED"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "ci/buildspec_${var.repo.branch}_backend.yml"
+  }
+
+  build_timeout = "120"
+}
