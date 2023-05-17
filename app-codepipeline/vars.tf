7635586cb8831fca2966f2fb5769156fba00f968
isaac g <drumsound@google.com>
2023-05-17 00:09:54 +0000

    ci: change var name

--- app-codepipeline/vars.tf
+++ app-codepipeline/vars.tf
@@ -70,9 +70,9 @@ variable "deploy_env_vars" {
   description = "Map of <env name>: <env value> that is injected as environment variables when deploying the services"
 }
 
-variable "pgpass_arn_key" {
-  type = map(string)
-  description = "Map that includes the ARN and JSON key for the pgpass secret in secret manager for the backend db"
+variable "db_creds_arn" {
+  type        = string
+  description = "The ARN for the backend database secrets in secret manager."
 }
 
 variable "codebuild_vpc_id" {
