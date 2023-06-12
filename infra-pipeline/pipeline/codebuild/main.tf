2c8801d5083e22a6f825b933597a0421ba50facd
Chris Casto <chriscasto@google.com>
2023-06-12 19:53:08 +0000

    fix: update build project name

--- infra-pipeline/pipeline/codebuild/main.tf
+++ infra-pipeline/pipeline/codebuild/main.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 resource "aws_codebuild_project" "project" {
-  name          = "${var.name_prefix}-build-${var.name}"
+  name          = local.qualified_name
   description   = "Deployment for ${var.name_prefix} into environment ${var.name}"
   build_timeout = 60
   service_role  = aws_iam_role.codebuild.arn
