d06d8de944a6dbb5b58ddd26e64c3ad222a2e3e4
Chris Casto <chriscasto@google.com>
2023-06-13 23:50:57 +0000

    feat: use route 53 aliases

--- bloom-instance/service/dns.tf
+++ bloom-instance/service/dns.tf
@@ -4,17 +4,22 @@ locals {
     for listener_name, listener in alb.listeners : {
       for domain in listener.domains : "${local.name}-${alb_name}-${listener_name}-${domain}" => {
         name   = domain
-        type   = "CNAME"
+        type   = "A"
         values = [var.alb_map[alb_name].dns_name]
-        ttl    = var.dns.default_ttl
+        #ttl    = var.dns.default_ttl
+
+        target = {
+          dns_name = var.alb_map[alb_name].dns_name
+          zone_id  = var.alb_map[alb_name].zone_id
+        }
       }
     }
     ]...)
   ]...)
 }
 
-module "alb_dns" {
-  source = "../dns/record"
+module "alb_aliases" {
+  source = "../dns/alias"
 
   for_each = local.alb_domain_map
 
