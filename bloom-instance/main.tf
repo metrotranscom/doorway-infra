c247998b9c7221cc5ee87c6668e79df6285d5d6e
Glenn Brown <gbrown@bayareametro.gov>
2024-01-24 14:53:30 -0800

    adding us-east-1 provider

--- bloom-instance/main.tf
+++ bloom-instance/main.tf
@@ -18,7 +18,12 @@ provider "aws" {
   region = var.aws_region
   default_tags { tags = local.default_tags }
 }
-
+# This provider is to create the certs in us-east-1 so that it can be used by cloudfront
+provider "aws" {
+  region = "us-east-1"
+  alias = "use1"
+  default_tags { tags = local.default_tags }
+}
 
 # Needed to resolve current AWS Account ID for policy documents
 data "aws_caller_identity" "current" {}
