1896e0760d1f5f05676f54de3dbf6ddb688ef203
Glenn Brown <gbrown@bayareametro.gov>
2024-01-25 13:28:53 -0800

    trying to require in the cert module

--- bloom-instance/cert/main.tf
+++ bloom-instance/cert/main.tf
@@ -1,4 +1,13 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.7.0"
+      configuration_aliases = [ aws.use1 ]
+    }
+  }
+}
 locals {
   # DNS records required for cert validation
   validation_records = {
