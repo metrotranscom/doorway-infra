c50e1040fa69b513f442bea7ff62e06db60b78cf
Glenn Brown <gbrown@bayareametro.gov>
2024-01-25 09:59:53 -0800

    need to destroy and recreate

--- bloom-instance/cert/main.tf
+++ bloom-instance/cert/main.tf
@@ -85,15 +85,15 @@ resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert : record.fqdn]
 }
-resource "aws_acm_certificate" "cloudfront-cert" {
-  provider = aws.use1
-  domain_name               = var.cert.domain
-  validation_method         = "DNS"
-  subject_alternative_names = var.cert.alt_names
-}
-resource "aws_acm_certificate_validation" "cloudfront-cert" {
-  provider = aws.use1
-  count                   = local.auto_validate ? 1 : 0
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert : record.fqdn]
-}
+# resource "aws_acm_certificate" "cloudfront-cert" {
+#   provider = aws.use1
+#   domain_name               = var.cert.domain
+#   validation_method         = "DNS"
+#   subject_alternative_names = var.cert.alt_names
+# }
+# resource "aws_acm_certificate_validation" "cloudfront-cert" {
+#   provider = aws.use1
+#   count                   = local.auto_validate ? 1 : 0
+#   certificate_arn         = aws_acm_certificate.cert.arn
+#   validation_record_fqdns = [for record in aws_route53_record.cert : record.fqdn]
+# }
