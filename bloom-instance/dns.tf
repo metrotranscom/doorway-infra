888c4a290ba8e93d9f80727c1ea29ad111dc617a
Glenn Brown <gbrown@bayareametro.gov>
2024-04-02 15:49:38 -0700

    temporarily removing DNS stuff

--- bloom-instance/dns.tf
+++ bloom-instance/dns.tf
@@ -1,13 +1,13 @@
 
 
-module "cloudfront_aliases" {
-  source = "./dns/alias"
+# module "cloudfront_aliases" {
+#   source = "./dns/alias"
 
-  for_each = local.cloudfront_domains
+#   for_each = local.cloudfront_domains
 
-  zones       = module.dns.zone_map
-  record_name = each.value
-  zone_id     = module.albs["public"].zone_id
-  target_name = module.albs["public"].dns_name
-}
+#   zones       = module.dns.zone_map
+#   record_name = each.value
+#   zone_id     = module.albs["public"].zone_id
+#   target_name = module.albs["public"].dns_name
+# }
 
