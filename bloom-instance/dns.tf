e44aa336883f334639fc8a645904ab84d610483e
Glenn Brown <gbrown@bayareametro.gov>
2024-04-02 16:42:09 -0700

    adding dns back

--- bloom-instance/dns.tf
+++ bloom-instance/dns.tf
@@ -11,3 +11,22 @@
 #   target_name = module.albs["public"].dns_name
 # }
 
+data "aws_route53_zone" "zone" {
+  name = "housingbayarea.mtc.ca.gov."
+
+}
+resource "aws_route53_record" "public" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = var.public_portal_domain
+  type    = "A"
+  records = [module.cloudfront.dns_name]
+
+}
+resource "aws_route53_record" "partners" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = var.partners_portal_domain
+  type    = "A"
+  records = [module.cloudfront.dns_name]
+
+}
+
