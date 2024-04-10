9287e124b866b31e573fdb7d3896116e0b2345d7
Glenn Brown <gbrown@bayareametro.gov>
2024-04-10 13:55:48 -0700

    vpcid

--- bloom-instance/apigw.tf
+++ bloom-instance/apigw.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_endpoint" "api_endpoint" {
-  vpc_id              = module.network.vpc.vpc_id
+  vpc_id              = module.network.vpc.id
   service_name        = "com.amazonaws.${local.aws_region}.execute-api"
   vpc_endpoint_type   = "Interface"
   security_group_ids  = [module.nlbs["api-nlb"].security_group.id]
