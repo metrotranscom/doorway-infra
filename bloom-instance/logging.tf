3fe87165cade6e3d10f4cfa18e6aa2d2c8162007
Glenn Brown <gbrown@bayareametro.gov>
2024-04-09 17:29:04 -0700

    temporarily removing conditions

--- bloom-instance/logging.tf
+++ bloom-instance/logging.tf
@@ -29,16 +29,16 @@ data "aws_iam_policy_document" "accessdocument" {
       aws_s3_bucket.logging_bucket.arn,
       "${aws_s3_bucket.logging_bucket.arn}/*"
     ]
-    condition {
-      test     = "ForAnyValue:StringEquals"
-      variable = "aws:SourceAccount"
-      values   = [local.aws_account]
-    }
-    condition {
-      test     = "ForAnyValue:ArnLike"
-      variable = "aws:SourceArn"
-      values   = ["arn:aws:logs:${local.aws_region}:${local.aws_account}:*"]
-    }
+    # condition {
+    #   test     = "ForAnyValue:StringEquals"
+    #   variable = "aws:SourceAccount"
+    #   values   = [local.aws_account]
+    # }
+    # condition {
+    #   test     = "ForAnyValue:ArnLike"
+    #   variable = "aws:SourceArn"
+    #   values   = ["arn:aws:logs:${local.aws_region}:${local.aws_account}:*"]
+    # }
 
   }
   statement {
@@ -52,21 +52,21 @@ data "aws_iam_policy_document" "accessdocument" {
       aws_s3_bucket.logging_bucket.arn,
       "${aws_s3_bucket.logging_bucket.arn}/*"
     ]
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceAccount"
-      values   = [local.aws_account]
-    }
-    condition {
-      test     = "ArnLike"
-      variable = "aws:SourceArn"
-      values   = ["arn:aws:logs:${local.aws_region}:${local.aws_account}:*"]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "s3:x-amz-acl"
-      values   = ["bucket-owner-full-control"]
-    }
+    # condition {
+    #   test     = "StringEquals"
+    #   variable = "aws:SourceAccount"
+    #   values   = [local.aws_account]
+    # }
+    # condition {
+    #   test     = "ArnLike"
+    #   variable = "aws:SourceArn"
+    #   values   = ["arn:aws:logs:${local.aws_region}:${local.aws_account}:*"]
+    # }
+    # condition {
+    #   test     = "StringEquals"
+    #   variable = "s3:x-amz-acl"
+    #   values   = ["bucket-owner-full-control"]
+    # }
 
   }
 
