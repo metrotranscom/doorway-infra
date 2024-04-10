5b5319a639484b353760539dd8eedf00fca51182
Glenn Brown <gbrown@bayareametro.gov>
2024-04-09 17:17:28 -0700

    making s3 bucket access changes

--- bloom-instance/logging.tf
+++ bloom-instance/logging.tf
@@ -5,70 +5,100 @@ resource "aws_s3_bucket" "logging_bucket" {
   #force_destroy = true
 
 }
+data "aws_iam_policy_document" "accessdocument" {
+  statement {
+    sid = "BucketLoggingAccess"
+    principals {
+      type        = "AWS"
+      identifiers = [local.elb_service_account_arn]
+    }
+    actions = ["s3:PutObject", ]
+    resources = [
+      aws_s3_bucket.logging_bucket.arn,
+      "${aws_s3_bucket.logging_bucket.arn}/*"
+    ]
+  }
+  statement {
+    sid = "AWSLogDeliveryAclCheck"
+    principals {
+      type        = "AWS"
+      identifiers = [local.elb_service_account_arn]
+    }
+    actions = ["s3:GetBucketAcl", ]
+    resources = [
+      aws_s3_bucket.logging_bucket.arn,
+      "${aws_s3_bucket.logging_bucket.arn}/*"
+    ]
+    condition {
+      test     = "ForAnyValue:StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [local.aws_account]
+    }
+    condition {
+      test     = "ForAnyValue:ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:logs:${local.aws_region}:${local.aws_account}:*"]
+    }
+
+  }
+  statement {
+    sid = "AWSLogDeliveryAclCheck"
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+    actions = ["s3:GetBucketAcl", ]
+    resources = [
+      aws_s3_bucket.logging_bucket.arn,
+      "${aws_s3_bucket.logging_bucket.arn}/*"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [local.aws_account]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:logs:${local.aws_region}:${local.aws_account}:*"]
+    }
+
+  }
+  statement {
+    sid = "AWSLogDeliveryWrite"
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+    actions = ["s3:PutObject", ]
+    resources = [
+      aws_s3_bucket.logging_bucket.arn,
+      "${aws_s3_bucket.logging_bucket.arn}/*"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [local.aws_account]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:logs:${local.aws_region}:${local.aws_account}:*"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+
+  }
+
+
+}
 
 resource "aws_s3_bucket_policy" "log_bucket_policy" {
   bucket = aws_s3_bucket.logging_bucket.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = {
-      Sid = "AllowWritesToLogBucket"
-      Action = [
-        "s3:PutObject",
-      ]
-      Effect = "Allow"
-      Principal = {
-        AWS = local.elb_service_account_arn
-      }
-      Resource = "${aws_s3_bucket.logging_bucket.arn}/*",
-    },
-    Statement = {
-      Sid = "AWSLogDeliveryAclCheck"
-      Action = [
-        "s3:GetBucketAcl",
-      ]
-      Effect = "Allow"
-      Principal = {
-        Service = "delivery.logs.amazonaws.com"
-      }
-      Resource = "${aws_s3_bucket.logging_bucket.arn}/*",
-      condition = {
-        test     = "ForAnyValue:StringEquals"
-        variable = "aws:SourceAccount"
-        values   = [local.aws_account]
-      },
-      condition = {
-        test     = "ForAnyValue:ArnLike"
-        variable = "aws:SourceArn"
-        values   = ["arn:aws:logs:${local.aws_region}:${local.aws_account}:*"]
-      }
-    },
-    Statement = {
-      Sid = "AWSLogDeliveryAclCheck"
-      Action = [
-        "s3:PutObject",
-      ]
-      Effect = "Allow"
-      Principal = {
-        Service = "delivery.logs.amazonaws.com"
-      }
-      Resource = "${aws_s3_bucket.logging_bucket.arn}/*",
-      condition = {
-        test     = "ForAnyValue:StringEquals"
-        variable = "s3:x-amz-acl"
-        values   = ["bucket-owner-full-control"]
-      },
-      condition = {
-        test     = "ForAnyValue:StringEquals"
-        variable = "aws:SourceAccount"
-        values   = [local.aws_account]
-      },
-      condition = {
-        test     = "ForAnyValue:ArnLike"
-        variable = "aws:SourceArn"
-        values   = ["arn:aws:logs:${local.aws_region}:${local.aws_account}:*"]
-      }
-    }
+  policy = data.aws_iam_policy_document.accessdocument
 
-  })
 }
