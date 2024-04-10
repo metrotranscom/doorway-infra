810281b75edef5caad0a41efb5c75b3e7b72fbb8
Glenn Brown <gbrown@bayareametro.gov>
2024-04-10 13:52:09 -0700

    api-nlb

--- bloom-instance/apigw.tf
+++ bloom-instance/apigw.tf
@@ -2,7 +2,7 @@ resource "aws_vpc_endpoint" "api_endpoint" {
   vpc_id              = module.network.vpc.vpc_id
   service_name        = "com.amazonaws.${local.aws_region}.execute-api"
   vpc_endpoint_type   = "Interface"
-  security_group_ids  = [module.nlbs["api"].security_group]
+  security_group_ids  = [module.nlbs["api-nlb"].security_group]
   private_dns_enabled = true
 
 
