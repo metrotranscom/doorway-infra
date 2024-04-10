e4bbb92700a2bc79fd7c5ee017170fbdd8423b46
Glenn Brown <gbrown@bayareametro.gov>
2024-04-10 13:48:33 -0700

    adding vpc endpoint

---
+++ bloom-instance/apigw.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc_endpoint" "api_endpoint" {
+  vpc_id              = module.network.vpc_id
+  service_name        = "com.amazonaws.${local.aws_region}.execute-api"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [module.nlbs["api"].security_group]
+  private_dns_enabled = true
+
+
+
+}
