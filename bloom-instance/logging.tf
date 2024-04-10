f33f42dc6ac1fe34a54a0ce410ba4744d95715ce
Glenn Brown <gbrown@bayareametro.gov>
2024-04-09 17:39:55 -0700

    trying another rename

--- bloom-instance/logging.tf
+++ bloom-instance/logging.tf
@@ -29,16 +29,16 @@ data "aws_iam_policy_document" "access_document" {
       aws_s3_bucket.logging_bucket.arn,
       "${aws_s3_bucket.logging_bucket.arn}/*"
     ]
-    # condition {
-    #   test     = "ForAnyValue:StringEquals"
-    #   variable = "aws:SourceAccount"
-    #   values   = [local.aws_account]
-    # }
-    # condition {
-    #   test     = "ForAnyValue:ArnLike"
-    #   variable = "aws:SourceArn"
-    #   values   = ["arn:aws:logs:${local.aws_region}:${local.aws_account}:*"]
-    # }
+    condition {
+      test     = "ForAnyValue:StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [local.aws_account]
+    }
+    condition {
+      test     = "ForAnyValue:ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:logs:${local.aws_region}:${local.aws_account}:*"]
+    }
 
   }
   statement {
@@ -52,28 +52,28 @@ data "aws_iam_policy_document" "access_document" {
       aws_s3_bucket.logging_bucket.arn,
       "${aws_s3_bucket.logging_bucket.arn}/*"
     ]
-    # condition {
-    #   test     = "StringEquals"
-    #   variable = "aws:SourceAccount"
-    #   values   = [local.aws_account]
-    # }
-    # condition {
-    #   test     = "ArnLike"
-    #   variable = "aws:SourceArn"
-    #   values   = ["arn:aws:logs:${local.aws_region}:${local.aws_account}:*"]
-    # }
-    # condition {
-    #   test     = "StringEquals"
-    #   variable = "s3:x-amz-acl"
-    #   values   = ["bucket-owner-full-control"]
-    # }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [local.aws_account]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:logs:${local.aws_region}:${local.aws_account}:*"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
 
   }
 
 
 }
 
-resource "aws_s3_bucket_policy" "log_bucket_policy" {
+resource "aws_s3_bucket_policy" "log_policy" {
   bucket = aws_s3_bucket.logging_bucket.id
 
   policy = data.aws_iam_policy_document.access_document.json
