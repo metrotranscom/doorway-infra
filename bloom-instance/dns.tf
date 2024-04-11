7ee21fe3a177964d072ce63fd3aaf4dccb19d63c
Glenn Brown <gbrown@bayareametro.gov>
2024-04-11 13:46:13 -0700

    creating internal dns for api

--- bloom-instance/dns.tf
+++ bloom-instance/dns.tf
@@ -3,8 +3,9 @@ data "aws_route53_zone" "zone" {
   name = "housingbayarea.mtc.ca.gov."
 
 }
+# terraform can't seem to find private zones w/ zone name unfortunately
 data "aws_route53_zone" "zone_int" {
-  name = "housingbayarea.mtc.ca.int."
+  zone_id = "Z04687811M3N0Y9KL9A5F"
 
 }
 resource "aws_route53_record" "public" {
@@ -28,7 +29,7 @@ resource "aws_route53_record" "partners" {
   }
 }
 resource "aws_route53_record" "api" {
-  zone_id = data.aws_route53_zone.zone.zone_id
+  zone_id = data.aws_route53_zone.zone_int.zone_id
   name    = var.backend_api_domain
   type    = "A"
   alias {
