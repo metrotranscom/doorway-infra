0e80736e5e18966d156d8b4d86b5a2a4ef4f9a5b
Glenn Brown <gbrown@bayareametro.gov>
2024-04-13 19:47:04 -0700

    adding method response

--- bloom-instance/apigw.tf
+++ bloom-instance/apigw.tf
@@ -42,3 +42,10 @@ resource "aws_api_gateway_integration" "global_integration" {
   connection_id           = aws_api_gateway_vpc_link.vpclink.id
 
 }
+resource "aws_api_gateway_method_response" "good_response" {
+  resource_id = aws_api_gateway_resource.global.id
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  http_method = "ANY"
+  status_code = 200
+
+}
