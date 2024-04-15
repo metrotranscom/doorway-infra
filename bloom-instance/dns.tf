c9f40c90fbf6f8304d04c6e3172bd1b5e54eda43
Glenn Brown <gbrown@bayareametro.gov>
2024-04-15 14:25:32 -0700

    temporarily excluding api and nlb

--- bloom-instance/dns.tf
+++ bloom-instance/dns.tf
@@ -29,15 +29,15 @@ resource "aws_route53_record" "partners" {
   }
 }
 
-resource "aws_route53_record" "api" {
-  zone_id = data.aws_route53_zone.zone.zone_id
-  name    = var.backend_api_domain
-  type    = "A"
-  alias {
-    name                   = aws_api_gateway_domain_name.apigw.regional_domain_name
-    zone_id                = aws_api_gateway_domain_name.apigw.regional_zone_id
-    evaluate_target_health = false
-  }
+# resource "aws_route53_record" "api" {
+#   zone_id = data.aws_route53_zone.zone.zone_id
+#   name    = var.backend_api_domain
+#   type    = "A"
+#   alias {
+#     name                   = aws_api_gateway_domain_name.apigw.regional_domain_name
+#     zone_id                = aws_api_gateway_domain_name.apigw.regional_zone_id
+#     evaluate_target_health = false
+#   }
 
-}
+# }
 
--- bloom-instance/nlb.tf
+++ bloom-instance/nlb.tf
@@ -1,22 +1,22 @@
 
-module "nlbs" {
-  source   = "./nlb"
-  for_each = { for name, nlb in var.nlbs : name => nlb }
+# module "nlbs" {
+#   source   = "./nlb"
+#   for_each = { for name, nlb in var.nlbs : name => nlb }
 
-  vpc_id     = module.network.vpc.id
-  log_bucket = aws_s3_bucket.logging_bucket.bucket
+#   vpc_id     = module.network.vpc.id
+#   log_bucket = aws_s3_bucket.logging_bucket.bucket
 
-  # Pass in available subnets so the ALB can pick which subnets to deploy into based on subnet group
-  # Also used by listener module to allow IPs from subnets in allowed_subnets
-  subnets = module.network.subnets
+#   # Pass in available subnets so the ALB can pick which subnets to deploy into based on subnet group
+#   # Also used by listener module to allow IPs from subnets in allowed_subnets
+#   subnets = module.network.subnets
 
 
-  name_prefix     = local.qualified_name_prefix
-  name            = each.key
-  enable_logging  = each.value.enable_logging
-  internal        = each.value.internal
-  listeners       = each.value.listeners
-  subnet_group    = each.value.subnet_group
-  certificate_arn = module.certs[each.value.default_cert].arn
-  alb_arn         = module.albs["api"].alb.arn
-}
+#   name_prefix     = local.qualified_name_prefix
+#   name            = each.key
+#   enable_logging  = each.value.enable_logging
+#   internal        = each.value.internal
+#   listeners       = each.value.listeners
+#   subnet_group    = each.value.subnet_group
+#   certificate_arn = module.certs[each.value.default_cert].arn
+#   alb_arn         = module.albs["api"].alb.arn
+# }
