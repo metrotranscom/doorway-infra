0725833af53c2a6668002af8cfdd81d0889b9c86
Glenn Brown <gbrown@bayareametro.gov>
2024-04-15 15:03:30 -0700

    fixing method id

--- bloom-instance/apigw.tf
+++ bloom-instance/apigw.tf
@@ -78,7 +78,7 @@ resource "aws_api_gateway_integration" "global_integration" {
 }
 resource "aws_api_gateway_integration_response" "integration_response" {
   resource_id = aws_api_gateway_resource.global.id
-  http_method = "ANY"
+  http_method = aws_api_gateway_method.method.id
   rest_api_id = aws_api_gateway_rest_api.apigw.id
   status_code = 200
 
@@ -86,7 +86,7 @@ resource "aws_api_gateway_integration_response" "integration_response" {
 resource "aws_api_gateway_method_response" "good_response" {
   resource_id = aws_api_gateway_resource.global.id
   rest_api_id = aws_api_gateway_rest_api.apigw.id
-  http_method = "ANY"
+  http_method = aws_api_gateway_method.method.id
   status_code = 200
 
 }
