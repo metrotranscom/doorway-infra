c3b1c0bf40b077d2dfda15e5186859985fccc3d3
Glenn Brown <gbrown@bayareametro.gov>
2024-04-16 10:42:03 -0700

    renaming sgs to environment

--- bloom-instance/sg.tf
+++ bloom-instance/sg.tf
@@ -1,10 +1,10 @@
 resource "aws_security_group" "local_https_only" {
-  name        = "local_https"
+  name        = "${local.qualified_name_prefix}_local_https"
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = module.network.vpc.id
 
   tags = {
-    Name = "local_https"
+    Name = "${local.qualified_name_prefix}_local_https"
   }
 }
 # Create ingress rules for each set of allowed CIDR blocks
@@ -29,12 +29,12 @@ resource "aws_vpc_security_group_egress_rule" "local_https_egress" {
 }
 
 resource "aws_security_group" "ecs_sg" {
-  name        = "ecs_sg"
+  name        = "${local.qualified_name_prefix}__ecs_sg"
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = module.network.vpc.id
 
   tags = {
-    Name = "ecs_sg"
+    Name = "${local.qualified_name_prefix}__ecs_sg"
   }
 }
 # Create ingress rules for each set of allowed CIDR blocks
