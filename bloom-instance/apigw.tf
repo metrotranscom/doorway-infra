217972c76a7ffcc15af9152afcfded2cd1f8c584
Glenn Brown <gbrown@bayareametro.gov>
2024-04-16 15:21:38 -0700

    removing duplicate response

--- bloom-instance/apigw.tf
+++ bloom-instance/apigw.tf
@@ -101,13 +101,7 @@ resource "aws_api_gateway_integration_response" "integration_response" {
   status_code = 200
 
 }
-resource "aws_api_gateway_method_response" "good_response" {
-  resource_id = aws_api_gateway_resource.global.id
-  rest_api_id = aws_api_gateway_rest_api.apigw.id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = 200
 
-}
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.apigw.id
 }
