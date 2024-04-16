0802e813ec8e2862a18d31189ddf8ccb456a10b3
Glenn Brown <gbrown@bayareametro.gov>
2024-04-16 10:33:03 -0700

    ecs security group and outbound rules

--- bloom-instance/sg.tf
+++ bloom-instance/sg.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "local_https_only" {
   }
 }
 # Create ingress rules for each set of allowed CIDR blocks
-resource "aws_vpc_security_group_ingress_rule" "ingress" {
+resource "aws_vpc_security_group_ingress_rule" "local_https_ingress" {
   for_each = { for cidr in local.local_cidrs : cidr => cidr }
 
   security_group_id = aws_security_group.local_https_only.id
@@ -18,5 +18,29 @@ resource "aws_vpc_security_group_ingress_rule" "ingress" {
   to_port     = 443
   ip_protocol = "TCP"
 
+
+}
+# Create ingress rules for each set of allowed CIDR blocks
+resource "aws_vpc_security_group_egress_rule" "local_https_egress" {
+  security_group_id = aws_security_group.local_https_only.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs_sg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = module.network.vpc.id
+
+  tags = {
+    Name = "ecs_sg"
+  }
+}
+# Create ingress rules for each set of allowed CIDR blocks
+resource "aws_vpc_security_group_egress_rule" "ecs_egress" {
+  security_group_id = aws_security_group.ecs_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 
 }
