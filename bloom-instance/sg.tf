b706e048331675276bbb3a556c15f52918b60e7a
Glenn Brown <gbrown@bayareametro.gov>
2024-04-16 10:54:40 -0700

    ingress rules for ECS

--- bloom-instance/sg.tf
+++ bloom-instance/sg.tf
@@ -37,6 +37,62 @@ resource "aws_security_group" "ecs_sg" {
     Name = "${local.qualified_name_prefix}__ecs_sg"
   }
 }
+
+# Create ingress rules for each set of allowed CIDR blocks
+resource "aws_vpc_security_group_ingress_rule" "local_ecs_public_service_ingress" {
+  for_each = { for cidr in local.local_cidrs : cidr => cidr }
+
+  security_group_id = aws_security_group.ecs_sg.id
+
+  cidr_ipv4   = each.value
+  from_port   = var.public_sites[0].port
+  to_port     = var.public_sites[0].port
+  ip_protocol = "TCP"
+
+
+}
+# Create ingress rules for each set of allowed CIDR blocks
+resource "aws_vpc_security_group_ingress_rule" "local_ecs_public_service_ingress" {
+  for_each = { for cidr in local.local_cidrs : cidr => cidr }
+
+  security_group_id = aws_security_group.ecs_sg.id
+
+  cidr_ipv4   = each.value
+  from_port   = var.partner_site.port
+  to_port     = var.partner_site.port
+  ip_protocol = "TCP"
+
+
+}
+# Create ingress rules for each set of allowed CIDR blocks
+resource "aws_vpc_security_group_ingress_rule" "local_ecs_public_service_ingress" {
+  for_each = { for cidr in local.local_cidrs : cidr => cidr }
+
+  security_group_id = aws_security_group.ecs_sg.id
+
+  cidr_ipv4   = each.value
+  from_port   = var.backend_api.port
+  to_port     = var.backend_api.port
+  ip_protocol = "TCP"
+
+
+}
+# Create ingress rules for each set of allowed CIDR blocks
+resource "aws_vpc_security_group_egress_rule" "ecs_egress" {
+  security_group_id = aws_security_group.ecs_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+
+}
+resource "aws_security_group" "db_sg" {
+  name        = "${local.qualified_name_prefix}__db_sg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = module.network.vpc.id
+
+  tags = {
+    Name = "${local.qualified_name_prefix}__db_sg"
+  }
+}
 # Create ingress rules for each set of allowed CIDR blocks
 resource "aws_vpc_security_group_egress_rule" "ecs_egress" {
   security_group_id = aws_security_group.ecs_sg.id
