bc337ae4aa6a6beeee5feb616860dfb2b64f8ab8
Glenn Brown <gbrown@bayareametro.gov>
2024-04-18 11:41:52 -0700

    another redeploy method

--- bloom-instance/apigw.tf
+++ bloom-instance/apigw.tf
@@ -97,21 +97,9 @@ resource "aws_api_gateway_integration" "global_integration" {
 resource "aws_api_gateway_deployment" "deployment" {
 
   rest_api_id = aws_api_gateway_rest_api.apigw.id
-  triggers = {
-    # NOTE: The configuration below will satisfy ordering considerations,
-    #       but not pick up all future REST API changes. More advanced patterns
-    #       are possible, such as using the filesha1() function against the
-    #       Terraform configuration file(s) or removing the .id references to
-    #       calculate a hash against whole resources. Be aware that using whole
-    #       resources will show a difference after the initial implementation.
-    #       It will stabilize to only change when resources change afterwards.
-    redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.global.id,
-      aws_api_gateway_method.method.id,
-      aws_api_gateway_integration.global_integration.id,
-      aws_api_gateway_method_response.enable_cors.id,
-      aws_api_gateway_integration_response.cors_int_response.id,
-    ]))
+  #this forces a redeploy of the API every run
+  variables = {
+    deployed_at = timestamp()
   }
 }
 resource "aws_api_gateway_stage" "main" {
