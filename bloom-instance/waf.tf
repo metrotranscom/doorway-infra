91f11715e70809341ed050226dea36c936d985e2
Glenn Brown <gbrown@bayareametro.gov>
2024-04-19 14:34:06 -0700

    adding api acl

--- bloom-instance/waf.tf
+++ bloom-instance/waf.tf
@@ -42,11 +42,6 @@ resource "aws_wafv2_web_acl" "cloudfront_acl" {
     }
   }
 
-  tags = {
-    Tag1 = "Value1"
-    Tag2 = "Value2"
-  }
-
   visibility_config {
     cloudwatch_metrics_enabled = true
     metric_name                = "${local.qualified_name_prefix}-cloudfront-acl"
@@ -55,3 +50,54 @@ resource "aws_wafv2_web_acl" "cloudfront_acl" {
   provider = aws.use1
 }
 
+resource "aws_wafv2_web_acl" "apigw_acl" {
+  name        = "${local.qualified_name_prefix}-waf-acl"
+  description = "ACL for the waf"
+  scope       = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "common-rule-set"
+    priority = 1
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+
+        scope_down_statement {
+          geo_match_statement {
+            country_codes = ["US"]
+          }
+        }
+      }
+    }
+
+
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${local.qualified_name_prefix}-cloudfront-acl-common_ruleset"
+      sampled_requests_enabled   = true
+    }
+  }
+
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${local.qualified_name_prefix}-cloudfront-acl"
+    sampled_requests_enabled   = true
+  }
+  provider = aws
+}
+resource "aws_wafv2_web_acl_association" "example" {
+  resource_arn = aws_api_gateway_stage.main.arn
+  web_acl_arn  = aws_wafv2_web_acl.apigw_acl.arn
+}
