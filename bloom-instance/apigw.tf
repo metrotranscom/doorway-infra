84b3b121d6e4e9248072c975ed8e35966f92d1b4
Glenn Brown <gbrown@bayareametro.gov>
2024-04-19 10:40:31 -0700

    trying this with HTTP_PROXY

--- bloom-instance/apigw.tf
+++ bloom-instance/apigw.tf
@@ -55,51 +55,51 @@ resource "aws_api_gateway_method_settings" "method_settings" {
 
 }
 
-resource "aws_api_gateway_method_response" "enable_cors" {
-  for_each    = local.http_rc_map
-  status_code = each.key
-  rest_api_id = aws_api_gateway_rest_api.apigw.id
-  resource_id = aws_api_gateway_resource.global.id
-  http_method = "ANY"
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers"     = true,
-    "method.response.header.Access-Control-Allow-Methods"     = true,
-    "method.response.header.Access-Control-Allow-Origin"      = true,
-    "method.response.header.Access-Control-Allow-Credentials" = true
+# resource "aws_api_gateway_method_response" "enable_cors" {
+#   for_each    = local.http_rc_map
+#   status_code = each.key
+#   rest_api_id = aws_api_gateway_rest_api.apigw.id
+#   resource_id = aws_api_gateway_resource.global.id
+#   http_method = "ANY"
+#   response_parameters = {
+#     "method.response.header.Access-Control-Allow-Headers"     = true,
+#     "method.response.header.Access-Control-Allow-Methods"     = true,
+#     "method.response.header.Access-Control-Allow-Origin"      = true,
+#     "method.response.header.Access-Control-Allow-Credentials" = true
 
-  }
-}
-resource "aws_api_gateway_integration_response" "cors_int_response" {
-  for_each          = local.http_rc_map
-  resource_id       = aws_api_gateway_resource.global.id
-  rest_api_id       = aws_api_gateway_rest_api.apigw.id
-  http_method       = "ANY"
-  status_code       = each.key
-  selection_pattern = each.key
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Set-Cookie'",
-    "method.response.header.Access-Control-Allow-Methods"     = "'GET,OPTIONS,POST,PUT'",
-    "method.response.header.Access-Control-Allow-Origin"      = "'*'"
-    "method.response.header.Access-Control-Allow-Credentials" = "'true'"
+#   }
+# }
+# resource "aws_api_gateway_integration_response" "cors_int_response" {
+#   for_each          = local.http_rc_map
+#   resource_id       = aws_api_gateway_resource.global.id
+#   rest_api_id       = aws_api_gateway_rest_api.apigw.id
+#   http_method       = "ANY"
+#   status_code       = each.key
+#   selection_pattern = each.key
+#   response_parameters = {
+#     "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Set-Cookie'",
+#     "method.response.header.Access-Control-Allow-Methods"     = "'GET,OPTIONS,POST,PUT'",
+#     "method.response.header.Access-Control-Allow-Origin"      = "'*'"
+#     "method.response.header.Access-Control-Allow-Credentials" = "'true'"
 
-  }
-}
+#   }
+# }
 
 resource "aws_api_gateway_integration" "global_integration" {
   rest_api_id = aws_api_gateway_rest_api.apigw.id
   resource_id = aws_api_gateway_resource.global.id
 
-  type                    = "HTTP"
+  type                    = "HTTP_PROXY"
   uri                     = "https://${aws_api_gateway_domain_name.apigw.domain_name}/{proxy}"
   http_method             = "ANY"
   integration_http_method = "ANY"
   connection_type         = "VPC_LINK"
   connection_id           = aws_api_gateway_vpc_link.vpclink.id
-  request_parameters = {
-    "integration.request.path.proxy"              = "method.request.path.proxy"
-    "integration.request.header.jurisdictionName" = "method.request.header.jurisdictionName"
-    "integration.request.header.Host"             = "method.request.header.Host"
-  }
+  # request_parameters = {
+  #   "integration.request.path.proxy"              = "method.request.path.proxy"
+  #   "integration.request.header.jurisdictionName" = "method.request.header.jurisdictionName"
+  #   "integration.request.header.Host"             = "method.request.header.Host"
+  # }
 
 }
 
