f1c1be48f5ea0a6b4859da178982e1a27ef8b295
Glenn Brown <gbrown@bayareametro.gov>
2024-05-16 15:30:09 -0700

    trying one at a time

--- bloom-instance/service/partner-site/listener.tf
+++ bloom-instance/service/partner-site/listener.tf
@@ -1,18 +1,18 @@
 
 
 
-resource "aws_lb_listener_rule" "static" {
-  listener_arn = var.alb_arn
-  priority     = 200
+# resource "aws_lb_listener_rule" "static" {
+#   listener_arn = var.alb_arn
+#   priority     = 200
 
-  action {
-    type             = "forward"
-    target_group_arn = module.service.target_group.arn
-  }
+#   action {
+#     type             = "forward"
+#     target_group_arn = module.service.target_group.arn
+#   }
 
-  condition {
-    host_header {
-      values = var.site_urls
-    }
-  }
-}
+#   condition {
+#     host_header {
+#       values = var.site_urls
+#     }
+#   }
+# }
