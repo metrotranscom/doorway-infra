ea64ca210b1ff8303e39461e6d3d96952891d71e
Glenn Brown <gbrown@bayareametro.gov>
2024-09-19 10:47:05 -0700

    fix: fixing formatting and top level permissions

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Doorway Infrastructure MonoRepo
+
 Each of the subfolders are their own project using [AWS CDK](https://aws.amazon.com/cdk/)
 Each project will have its own github actions assigned to build and will trigger only when there are modifications to that sub-folder
 They each deploy a discrete but large section of the necessary AWS framework.
 Currently in this repo:
+
 - A network load balancer
 - SSM Parameters to store VPC and subnet information necessary to create the load balancer
--- doorway-network/lib/doorway-network-stack.ts
+++ doorway-network/lib/doorway-network-stack.ts
@@ -1,34 +1,38 @@
-import * as cdk from 'aws-cdk-lib';
-import { ISubnet, Subnet, Vpc } from 'aws-cdk-lib/aws-ec2';
-import { NetworkLoadBalancer } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-import { StringListParameter, StringParameter } from 'aws-cdk-lib/aws-ssm';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { ISubnet, Subnet, Vpc } from "aws-cdk-lib/aws-ec2";
+import { NetworkLoadBalancer } from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { StringListParameter, StringParameter } from "aws-cdk-lib/aws-ssm";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 export class DoorwayNetworkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const environment = process.env.ENVIRONMENT
-    const vpcId = StringParameter.fromStringParameterAttributes(this, 'vpcId', {
+    const environment = process.env.ENVIRONMENT;
+    const vpcId = StringParameter.fromStringParameterAttributes(this, "vpcId", {
       parameterName: `/doorway/${environment}/vpc/id`,
-    }).stringValue
-    const appSubnets = StringListParameter.fromListParameterAttributes(this, 'appSubnets', {
-      parameterName: `/doorway/${environment}/vpc/appSubnets`,
-    }).stringListValue
-    const vpc = Vpc.fromVpcAttributes(this, 'vpc', {
+    }).stringValue;
+    const appSubnets = StringListParameter.fromListParameterAttributes(
+      this,
+      "appSubnets",
+      {
+        parameterName: `/doorway/${environment}/vpc/appSubnets`,
+      },
+    ).stringListValue;
+    const vpc = Vpc.fromVpcAttributes(this, "vpc", {
       vpcId: vpcId,
-      availabilityZones: ['us-west-1a','us-west-1c']
-    })
+      availabilityZones: ["us-west-1a", "us-west-1c"],
+    });
     const subnets: ISubnet[] = appSubnets.map((subnetId) => {
-      return Subnet.fromSubnetAttributes(this, 'subnet', {
+      return Subnet.fromSubnetAttributes(this, "subnet", {
         subnetId: subnetId,
-      })
-    })
-    const nlb  = new NetworkLoadBalancer(this, 'apiNlb', {
+      });
+    });
+    const nlb = new NetworkLoadBalancer(this, "apiNlb", {
       vpc: vpc,
       internetFacing: false,
       vpcSubnets: {
-        subnets:subnets
-         }
-    })
+        subnets: subnets,
+      },
+    });
   }
 }
