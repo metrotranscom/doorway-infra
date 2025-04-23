34670d5cdb94ba951949aaf9913b4e116af87783
Glenn Brown <gbrown@bayareametro.gov>
2025-04-23 10:27:18 -0700

    disable cf

--- bloom-instance/cloudfront.tf
+++ bloom-instance/cloudfront.tf
@@ -1,15 +1,15 @@
 
-module "cloudfront" {
-  source = "./cloudfront"
+# module "cloudfront" {
+#   source = "./cloudfront"
 
 
-  name_prefix = local.qualified_name_prefix
-  name        = "cloudfront"
-  domains     = [var.public_portal_domain, "*.${var.public_portal_domain}"]
+#   name_prefix = local.qualified_name_prefix
+#   name        = "cloudfront"
+#   domains     = [var.public_portal_domain, "*.${var.public_portal_domain}"]
 
-  alb_map  = module.albs
-  cert_map = local.cert_map
+#   alb_map  = module.albs
+#   cert_map = local.cert_map
 
-  distribution = var.cloudfront
-  web_acl_id   = aws_wafv2_web_acl.cloudfront_acl.arn
-}
+#   distribution = var.cloudfront
+#   web_acl_id   = aws_wafv2_web_acl.cloudfront_acl.arn
+# }
