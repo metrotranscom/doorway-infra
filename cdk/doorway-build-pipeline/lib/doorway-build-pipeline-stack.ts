2466aa0402f0e13f8dcda891144ed837cd8dde52
Glenn Brown <gbrown@bayareametro.gov>
2025-07-28 16:47:14 -0700

    feat: all containers built

--- cdk/doorway-build-pipeline/lib/doorway-build-pipeline-stack.ts
+++ cdk/doorway-build-pipeline/lib/doorway-build-pipeline-stack.ts
@@ -5,19 +5,21 @@ import {
   CodeBuildAction,
   GitHubSourceAction,
 } from "aws-cdk-lib/aws-codepipeline-actions";
-import { PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { Repository } from "aws-cdk-lib/aws-ecr";
+import {
+  PolicyDocument,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
 import { Secret } from "aws-cdk-lib/aws-secretsmanager";
+import { Construct } from "constructs";
 import * as fs from "fs";
 import YAML from "yaml";
-
-import { Repository } from "aws-cdk-lib/aws-ecr";
-import { Construct } from "constructs";
 export class DoorwayBuildPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props: PipelineProps) {
     super(scope, id, props);
-    const ecrRepository = new Repository(this, "doorway-ecr-repository", {
-      repositoryName: "doorway/backend",
-    });
+
     // No need to create a repository for docker-hub if using pull-through cache
     // The pull-through cache repository should already be configured in the AWS console
 
@@ -38,9 +40,53 @@ export class DoorwayBuildPipelineStack extends Stack {
         resources: ["*"],
       }),
     );
-    const backendBuildspec = YAML.parse(
-      fs.readFileSync("./buildspec/backend.yaml", "utf8"),
-    );
+    const buildRole = new Role(this, "doorway-app-build-role", {
+      assumedBy: new ServicePrincipal("codebuild.amazonaws.com"),
+      managedPolicies: [
+        {
+          managedPolicyArn:
+            "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess",
+        },
+        {
+          managedPolicyArn: "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+        },
+      ],
+      inlinePolicies: {
+        ECRPolicies: new PolicyDocument({
+          statements: [
+            new PolicyStatement({
+              actions: [
+                "ecr:BatchGetImage",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchCheckLayerAvailability",
+              ],
+              resources: [
+                `arn:aws:ecr:${this.region}:${this.account}:repository/*`,
+              ],
+            }),
+            new PolicyStatement({
+              actions: ["secretsmanager:GetSecretValue"],
+              resources: [
+                `arn:aws:secretsmanager:${this.region}:${this.account}:secret:mtc/dockerHub*`,
+              ],
+            }),
+            new PolicyStatement({
+              actions: [
+                "cloudformation:*",
+                "ec2:*",
+                "ssm:*",
+                "codebuild:*",
+                "logs:*",
+                "iam:AssumeRole",
+                "iam:PassRole",
+              ],
+              resources: ["*"],
+            }),
+          ],
+        }),
+      },
+    });
+
     const dockerSecret = Secret.fromSecretNameV2(
       this,
       "dockerSecret",
@@ -62,95 +108,105 @@ export class DoorwayBuildPipelineStack extends Stack {
       oauthToken: githubSecret,
       owner: "metrotranscom",
       repo: "doorway",
-      branch: "main",
+      branch: "fix/update_image_location",
       output: sourceArtifact,
+    });
+    const configSourceArtifact = new Artifact("ConfigSourceArtifact");
+    const doorwayConfigSource = new GitHubSourceAction({
+      actionName: "ConfigSource",
+      oauthToken: githubSecret,
+      owner: "metrotranscom",
+      repo: "doorway-config",
+      branch: "feat/cdk-terraform",
+      output: configSourceArtifact,
     });
 
     const sourceStage = pipeline.addStage({
       stageName: "Source",
-      actions: [doorwaySource],
+      actions: [doorwaySource, doorwayConfigSource],
     });
-    const buildArtifact = new Artifact("BuildOutput");
-    const buildRole = new Role(this, "doorway-app-build-role", {
-      assumedBy: new ServicePrincipal("codebuild.amazonaws.com"),
-
-      managedPolicies: [
-        {
-          managedPolicyArn:
-            "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess",
-        },
-        { managedPolicyArn: "arn:aws:iam::aws:policy/AmazonS3FullAccess" },
-      ],
-    });
-    ecrRepository.grantPullPush(buildRole);
-
-    // Add permissions to use the ECR pull-through cache
-    // Add permissions for jq
-    buildRole.addToPolicy(
-      new PolicyStatement({
-        actions: [
-          "ecr:BatchGetImage",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchCheckLayerAvailability",
-        ],
-        resources: [`arn:aws:ecr:${this.region}:${this.account}:repository/*`],
-      }),
-    );
-    buildRole.addToPolicy(
-      new PolicyStatement({
-        actions: [
-          "cloudformation:*",
-          "ec2:*",
-          "ssm:*",
-          "codebuild:*",
-          "logs:*",
-          "iam:AssumeRole",
-          "iam:PassRole",
-        ],
-        resources: ["*"],
-      }),
-    );
-    buildRole.addToPolicy(
-      new PolicyStatement({
-        actions: ["secretsmanager:GetSecretValue"],
-        resources: [
-          dockerSecret,
-          `arn:aws:secretsmanager:${this.region}:${this.account}:secret:mtc/dockerHub*`,
-        ],
-      }),
-    );
-
-    const buildAction = new CodeBuildAction({
-      actionName: "Build",
-      input: sourceArtifact,
-      outputs: [buildArtifact],
-      project: new PipelineProject(this, "doorway-app-build-project", {
-        buildSpec: BuildSpec.fromObject(backendBuildspec),
-        environmentVariables: {
-          ECR_REGION: { value: this.region },
-          ECR_ACCOUNT_ID: { value: this.account },
-          ECR_NAMESPACE: { value: "doorway" },
-          DOCKER_HUB_SECRET_ARN: {
-            value: `arn:aws:secretsmanager:${this.region}:${this.account}:secret:mtc/dockerHub`,
-          },
-        },
-
-        role: buildRole,
-      }),
-    });
-    const ecrServer = Repository.fromRepositoryName(
-      this,
-      "ecrServer",
-      "mtc-core-ecr",
-    );
-
     const buildStage = pipeline.addStage({
       stageName: "Build",
-      actions: [buildAction],
+      actions: [
+        new DoorwayDockerBuild(this, `doorway-backend`, {
+          buildspec: "./buildspec/build_backend.yml",
+          imageName: "backend",
+          source: sourceArtifact,
+          configSource: configSourceArtifact,
+          role: buildRole,
+          dockerHubSecret: dockerSecret,
+        }),
+        new DoorwayDockerBuild(this, "doorway-import-listings", {
+          buildspec: "./buildspec/build_import_listings.yml",
+          imageName: "import-listings",
+          source: sourceArtifact,
+          configSource: configSourceArtifact,
+          role: buildRole,
+          dockerHubSecret: dockerSecret,
+        }),
+        new DoorwayDockerBuild(this, `doorway-partners`, {
+          buildspec: "./buildspec/build_partners.yml",
+          imageName: "partners",
+          source: sourceArtifact,
+          configSource: configSourceArtifact,
+          role: buildRole,
+          dockerHubSecret: dockerSecret,
+        }),
+        new DoorwayDockerBuild(this, "doorway-public", {
+          buildspec: "./buildspec/build_public.yml",
+          imageName: "public",
+          source: sourceArtifact,
+          configSource: configSourceArtifact,
+          role: buildRole,
+          dockerHubSecret: dockerSecret,
+        }),
+      ],
     });
   }
 }
 export interface PipelineProps extends StackProps {
   githubSecret: string;
   dockerHubSecret: string;
+}
+export interface DoorwayDockerBuildProps {
+  buildspec: string;
+  imageName: string;
+  source: Artifact;
+  configSource: Artifact;
+  role: Role;
+  dockerHubSecret: string;
+}
+export class DoorwayDockerBuild extends CodeBuildAction {
+  constructor(stack: Stack, id: string, props: DoorwayDockerBuildProps) {
+    super({
+      input: props.source,
+      extraInputs: [props.configSource],
+      outputs: [new Artifact(`${id}-BuildOutput`)],
+      actionName: `${id}-DockerBuild`,
+      project: new PipelineProject(
+        stack,
+        id,
+
+        {
+          buildSpec: BuildSpec.fromObject(
+            YAML.parse(fs.readFileSync(props.buildspec, "utf8")),
+          ),
+          environmentVariables: {
+            ECR_REGION: { value: stack.region },
+            ECR_ACCOUNT_ID: { value: stack.account },
+            ECR_NAMESPACE: { value: "doorway" },
+            IMAGE_NAME: { value: props.imageName },
+            DOCKER_HUB_SECRET_ARN: {
+              value: props.dockerHubSecret,
+            },
+          },
+          role: props.role,
+        },
+      ),
+    });
+    const repo = new Repository(stack, `${id}-ECRRepository`, {
+      repositoryName: `doorway/${props.imageName}`,
+    });
+    repo.grantPullPush(props.role);
+  }
 }
