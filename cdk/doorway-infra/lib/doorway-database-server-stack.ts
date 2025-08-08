3b9b8ce075a00f1e4ee596602102ae1ff74d9598
Glenn Brown <gbrown@bayareametro.gov>
2025-08-08 15:05:21 -0700

    fix: changing db name

--- cdk/doorway-infra/lib/doorway-database-server-stack.ts
+++ cdk/doorway-infra/lib/doorway-database-server-stack.ts
@@ -67,7 +67,7 @@ export class DoorwayDatabaseServerStack extends Stack {
         storageEncrypted: true,
         allocatedStorage: 20,
         storageType: StorageType.STANDARD,
-        databaseName: "bloom",
+        databaseName: "doorway",
         credentials: {
           username: "doorway",
         },
