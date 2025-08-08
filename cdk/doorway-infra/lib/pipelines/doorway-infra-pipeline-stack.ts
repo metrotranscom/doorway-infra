5a59d66110c32f7066461046faede3b6b07ec280
Glenn Brown <gbrown@bayareametro.gov>
2025-08-08 14:50:01 -0700

    fix: env variables

--- cdk/doorway-infra/lib/pipelines/doorway-infra-pipeline-stack.ts
+++ cdk/doorway-infra/lib/pipelines/doorway-infra-pipeline-stack.ts
@@ -122,7 +122,12 @@ export class DoorwayInfraPipelineStack extends Stack {
     const vpcId = Fn.importValue(`doorway-vpc-id-dev2`);
     const subnetId = Fn.importValue(`doorway-app-subnet-1-dev2`);
     const securityGroupId = Fn.importValue(`doorway-app-sg-dev2`);
-    const dbSecret = Fn.importValue(`doorwayDBSecret-dev2`);
+    const dbSecretArn = Fn.importValue(`doorwayDBSecret-dev2`);
+    const dbSecret = Secret.fromSecretCompleteArn(
+      this,
+      "dbSecret",
+      dbSecretArn,
+    );
     const vpc = Vpc.fromVpcAttributes(this, "vpc", {
       vpcId: vpcId,
       availabilityZones: Fn.importValue(`doorway-azs-dev2`).split(", "),
@@ -178,25 +183,24 @@ export class DoorwayInfraPipelineStack extends Stack {
         projectName: "DatabaseMigration",
         role: buildRole,
         env: {
-          DB_SECRET_ARN: dbSecret,
+          PGUSER: dbSecret.secretValueFromJson("username").toString(),
+          PGHOST: dbSecret.secretValueFromJson("host").toString(),
+          PGPASSWORD: dbSecret.secretValueFromJson("password").toString(),
+          PGPORT: dbSecret.secretValueFromJson("port").toString(),
+          DB_SECRET_ARN: dbSecret.secretArn,
           ECR_REGION: Aws.REGION,
           ECR_ACCOUNT_ID: Aws.ACCOUNT_ID,
           ECR_NAMESPACE: "doorway",
         },
         commands: [
           "echo 'Running database migration'",
-          "# Get database credentials (secrets not logged)",
-          "export DB_CREDS=$(aws secretsmanager get-secret-value --secret-id $DB_SECRET_ARN --query SecretString --output text 2>/dev/null)",
-          "export DB_HOST=$(echo $DB_CREDS | jq -r '.host' 2>/dev/null)",
-          "export DB_USER=$(echo $DB_CREDS | jq -r '.username' 2>/dev/null)",
-          "export DB_PASSWORD=$(echo $DB_CREDS | jq -r '.password' 2>/dev/null)",
-          "export DB_PORT=$(echo $DB_CREDS | jq -r '.port' 2>/dev/null)",
+
           'aws ecr get-login-password --region "${ECR_REGION}" | docker login --username AWS --password-stdin "${ECR_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com"',
           'export ECR_REPO="${ECR_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/${ECR_NAMESPACE}"',
           'export MIGRATION_IMAGE="${ECR_REPO}/backend:migrate-candidate"',
           'docker pull "${MIGRATION_IMAGE}"',
           'export MIGRATION_CMD="${MIGRATION_CMD:-db:migration:run}"',
-          "echo $PGHOST",
+
           'docker run --env PGUSER="${PGUSER}" --env PGPASSWORD="${PGPASSWORD}" --env PGHOST="${PGHOST}" --env PGDATABASE="${PGDATABASE}" --env PGPORT="${PGPORT}" --env MIGRATION_CMD="${MIGRATION_CMD}" "${MIGRATION_IMAGE}"',
         ],
 
