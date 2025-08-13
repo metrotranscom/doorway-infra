2c69d252e23c4c4012ab2c2e9caa3120bc813629
Glenn Brown <gbrown@bayareametro.gov>
2025-08-12 19:51:36 -0700

    fix: fatfinger

--- cdk/doorway-infra/lib/service_infra/doorway-parameters-stack.ts
+++ cdk/doorway-infra/lib/service_infra/doorway-parameters-stack.ts
@@ -168,7 +168,7 @@ export class DoorwayParametersStack extends cdk.Stack {
       parameterName: `/doorway/${props.environment}/public-portal/SHOW_PROFESSIONAL_PARTNERS`,
       stringValue: "TRUE",
     });
-    new StringParameter(this, "nextJSPortß", {
+    new StringParameter(this, "nextJSPort", {
       parameterName: `/doorway/${props.environment}/public-portal/NEXTJS_PORT`,
       stringValue: "3000",
     });
